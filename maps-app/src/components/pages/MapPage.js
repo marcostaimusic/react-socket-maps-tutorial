925d53dfb083395e1a0214ac5feac3d8789a8de7
IT Academy Local <formacio@PC-1062618.formacio.local>
2021-10-04 12:39:11 +0200

    adding onscreen lng and lat details

--- maps-app/src/components/pages/MapPage.js
+++ maps-app/src/components/pages/MapPage.js
@@ -4,7 +4,7 @@ import mapboxgl from "mapbox-gl";
 mapboxgl.accessToken =
   "pk.eyJ1IjoibWFyY29zdGFpbXVzaWMiLCJhIjoiY2t1Y2c4b3FzMGJzeDJxcnZ4YzBibHI3cCJ9.3V2bYl2BR-xtfYmGi6vF0g";
 
-const intialPosition = {
+const initialPosition = {
   lng: 5,
   lat: 34,
   zoom: 2,
@@ -13,19 +13,23 @@ const intialPosition = {
 export const MapPage = () => {
   const mapDiv = useRef();
   const [map, setMap] = useState();
+  const [coords, setCoords] = useState(initialPosition);
 
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapDiv.current,
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [intialPosition.lng, intialPosition.lat],
-      zoom: intialPosition.zoom,
+      center: [initialPosition.lng, initialPosition.lat],
+      zoom: initialPosition.zoom,
     });
 
     setMap(map);
   }, []);
   return (
     <>
+      <div className="infoWindow">
+        Lng: {coords.lng} | Lat: {coords.lat} | zoom: {coords.zoom}
+      </div>
       <div ref={mapDiv} className="mapContainer" />
     </>
   );
